ea9bf07b8dbc61faaf7c49739031e27166cb717f
Firepup Sixfifty <firepyp650@gmail.com>
2025-02-28 11:33:46 -0600

    Add CLI tool to look through the cache for a specified username

---
+++ find.py
@@ -0,0 +1,12 @@
+from cache import userMappings
+from sys import argv
+
+if len(argv) < 2:
+    exit("No username specified to lookup")
+
+argv.pop(0)
+username = " ".join(argv)
+
+for k, v in userMappings.items():
+    if username in v:
+        print(f"Found it! {k} ({v}")
